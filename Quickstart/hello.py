66e988524d991e491bb4d77886261214c78ff999
Tanishka Kohli <tanishka.kohli00@gmail.com>
2021-06-06 18:47:42 +0530

    day 2

--- Quickstart/hello.py
+++ Quickstart/hello.py
@@ -1,8 +1,47 @@
+from flask import url_for
+from logging import debug
 from flask import Flask
+from markupsafe import escape
 
 app = Flask(__name__)
 
 
-@app.route("/")
-def helloworld():
-    return "<p>Hello World!!!</p>"
+@app.route('/')
+def index():
+    return 'index'
+
+
+@app.route('/login')
+def login():
+    return 'login'
+
+
+@app.route('/user/<username>')
+def profile(username):
+    return f'{username}\'s profile'
+
+
+with app.test_request_context():
+    print(url_for('index'))
+    print(url_for('login'))
+    print(url_for('login', next='/'))
+    print(url_for('profile', username='John Doe'))
+
+
+# @app.route("/")
+# def hello():
+#     return "<h1>Index page</h1>"
+
+
+# @app.route("/hello")
+# def helloworld():
+#     return "<h1>Hello World!!!</h1>"
+
+
+# @app.route("/<name>")
+# def names(name):
+#     return f'hello, {escape(name)}!!'
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
